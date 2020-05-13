2dd233b436d8514a0e2e8a6c023e4ade8bdabd80
marshall405 <marshall.slemp@gmail.com>
2020-05-13 15:29:06 -0500

    Done.

--- app.rb
+++ app.rb
@@ -21,9 +21,30 @@ class App < Sinatra::Base
       str += phrase + " "
     end
     str 
-
-
   end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    word = params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5] +"."
+  end
+
+  get '/:operation/:number1/:number2' do
+    op =params[:operation]
+    num1 = params[:number1].to_i
+    num2 = params[:number2].to_i
+    result = 0
+    case op
+    when 'add'
+      result = num1 + num2
+    when 'subtract'
+      result = num1 - num2
+    when 'multiply'
+      result = num1 * num2
+    when 'divide'
+      result = num1 / num2
+    end
+    result.to_s
+  end
+
 
 
 end
